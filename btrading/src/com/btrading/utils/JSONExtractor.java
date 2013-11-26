e90cbbfefa43701a265afd82f8180b30076fb40f
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-11-26 20:12:02 +0800

    asd

--- btrading/src/com/btrading/utils/JSONExtractor.java
+++ btrading/src/com/btrading/utils/JSONExtractor.java
@@ -39,54 +39,13 @@ public class JSONExtractor {
         return sb.toString();
     }
 			
-	//THIS METHOD EXTRACTS THE LOGIN REQUEST DATA
-	public void ExtractLoginRequest(HttpResponse data) throws IllegalStateException, IOException, JSONException
+	
+	
+	//THIS METHODS EXTRACTS THE REQUESTED PRODUCTS DATA
+	public void ExtractAllProductRequest(HttpResponse data) throws IllegalStateException, IOException, JSONException
 	{
-		HttpEntity entity = data.getEntity();
-        // If the response does not enclose an entity, there is no need
-        // to worry about connection release
-        if (entity != null) {
-        	 
-            // A Simple JSON Response Read
-            InputStream instream = entity.getContent();
-            String result= convertStreamToString(instream);
-            Log.i("FULL LOGIN RESPONSE",result);
-
-            // A Simple JSONObject Creation
-            JSONObject json = null;
-			try {
-				json = new JSONObject(result);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-          //  Log.i("Praeda","<jsonobject>\n"+json.toString()+"\n</jsonobject>");
-
-            // A Simple JSONObject Parsing
-            JSONArray nameArray=json.names();
-            JSONArray valArray=json.toJSONArray(nameArray);
-//            for(int i=0;i<valArray.length();i++)
-//            {
-//                Log.i("Praeda","<jsonname"+i+">\n"+nameArray.getString(i)+"\n</jsonname"+i+">\n"
-//                        +"<jsonvalue"+i+">\n"+valArray.getString(i)+"\n</jsonvalue"+i+">");
-//            }
-
-            // A Simple JSONObject Value Pushing
-            //json.put("sample key", "sample value");
-          //  Log.i("Praeda","<jsonobject>\n"+json.toString()+"\n</jsonobject>");
-
-            // Closing the input stream will trigger connection release
-            instream.close();
-        }
+		
 	}
 	
-	//THIS METHODS EXTRACTS THE REGISTER USER REQUEST DATA
-	public void ExtractRegisterUserRequest(HttpResponse data) throws IllegalStateException, IOException, JSONException
-	{
-	}
 	
-	//THIS METHODS EXTRACTS THE UPDATE USER REQUEST DATA
-	public void ExtractUpdateUserRequest(HttpResponse data) throws IllegalStateException, IOException, JSONException
-	{
-	}
 }
