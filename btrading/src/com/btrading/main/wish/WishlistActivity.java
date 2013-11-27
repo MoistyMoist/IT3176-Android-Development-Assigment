f3219938fc8b01dbbfb450156902c2f22815beea
eddietcs <eddietcs@hotmail.com>
2013-11-27 16:16:10 +0800

    wish

--- btrading/src/com/btrading/main/wish/WishlistActivity.java
+++ btrading/src/com/btrading/main/wish/WishlistActivity.java
@@ -1,5 +1,6 @@
 package com.btrading.main.wish;
 
+import com.btrading.main.MainBaseActivity;
 import com.example.btrading.R;
 import com.example.btrading.R.layout;
 import com.example.btrading.R.menu;
@@ -7,23 +8,37 @@ import com.example.btrading.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class WishlistActivity extends Activity {
+public class WishlistActivity extends MainBaseActivity {
 
 	ListView lv_wish;
+	ArrayAdapter<CharSequence> adapter;
+	String[] wish_items = {"ABC Duck","TV controller","Water Bottle"};
+	
+	public WishlistActivity(){
+		super(R.string.title_activity_add_wishlist);
+	}
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_wish);
+		
+		lv_wish = (ListView) findViewById(R.id.lv_wish);
+		adapter = new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_dropdown_item, wish_items);
+		lv_wish.setAdapter(adapter);
 	}
-
+	
+	
+	
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.wishlist, menu);
 		return true;
 	}
-
+	*/
 }
