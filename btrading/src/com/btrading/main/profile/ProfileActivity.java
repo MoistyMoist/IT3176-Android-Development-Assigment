38ebda30e5fd945a8eb3221d0543caa3160d30e1
mohamad shahrikin b mohamad alias <shahrikinalias@gmail.com>
2014-01-04 13:27:56 +0800

    pookdw

--- btrading/src/com/btrading/main/profile/ProfileActivity.java
+++ btrading/src/com/btrading/main/profile/ProfileActivity.java
@@ -4,7 +4,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import com.btrading.httprequests.CreateUserRequest;
 import com.btrading.httprequests.RetrieveUserRequest;
+import com.btrading.httprequests.UpdateUserRequest;
 import com.btrading.models.User;
 import com.btrading.utils.StaticObjects;
 import com.example.btrading.R;
@@ -25,9 +27,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
-	
+
 	StaticObjects staticObjects;
-	EditText etEmail,etSex,etDob,etContact, nickName;
+	EditText etEmail, etSex, etDob, etContact, nickName;
 	TextView tvUserName;
 	Button editProfile, btnUpdate, btnClear;
 	SmartImageView imageView1;
@@ -36,101 +38,99 @@ public class ProfileActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_profile);
-		
+
 		tvUserName = (TextView) findViewById(R.id.tvUserName);
 		etEmail = (EditText) findViewById(R.id.etEmail);
 		etSex = (EditText) findViewById(R.id.etSex);
 		etDob = (EditText) findViewById(R.id.etDob);
 		etContact = (EditText) findViewById(R.id.etContact);
-		nickName=(EditText) findViewById(R.id.nickName);
-		editProfile=(Button) findViewById(R.id.btnEditProfile);
-		btnUpdate=(Button) findViewById(R.id.btnProfileUpdate);
-		btnClear=(Button) findViewById(R.id.btnClear);
+		nickName = (EditText) findViewById(R.id.nickName);
+		editProfile = (Button) findViewById(R.id.btnEditProfile);
+		btnUpdate = (Button) findViewById(R.id.btnProfileUpdate);
+		btnClear = (Button) findViewById(R.id.btnClear);
 		imageView1 = (SmartImageView) findViewById(R.id.imageView1);
-		
-		
-		tvUserName.setText(Integer.toString(StaticObjects.getCurrentUser().getUserID()));
+
+		tvUserName.setText(Integer.toString(StaticObjects.getCurrentUser()
+				.getUserID()));
 		etEmail.setText(StaticObjects.getCurrentUser().getEmail());
 		etSex.setText(StaticObjects.getCurrentUser().getSex());
 		etDob.setText(StaticObjects.getCurrentUser().getDob());
 		etContact.setText(StaticObjects.getCurrentUser().getContact());
 		nickName.setText(StaticObjects.getCurrentUser().getNickname());
-		
-        imageView1.setImageUrl(StaticObjects.getCurrentUser().getImageURL());
-		
-		editProfile.setOnClickListener(new OnClickListener(){
+
+		imageView1.setImageUrl(StaticObjects.getCurrentUser().getImageURL());
+
+		editProfile.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-			if(etEmail.isEnabled()==false){	
-			
-				btnUpdate.setVisibility(View.VISIBLE);
-				btnClear.setVisibility(View.VISIBLE);
-				tvUserName.setEnabled(true);
-				etEmail.setEnabled(true);
-				etSex.setEnabled(true);
-				tvUserName.setEnabled(true);
-				etDob.setEnabled(true);
-				etContact.setEnabled(true);
-				nickName.setEnabled(true);
-				
-				tvUserName.setBackgroundColor(0xffcccccc);
-				etEmail.setBackgroundColor(0xffcccccc);
-				etSex.setBackgroundColor(0xffcccccc);
-				tvUserName.setBackgroundColor(0xffcccccc);
-				etDob.setBackgroundColor(0xffcccccc);
-				etContact.setBackgroundColor(0xffcccccc);
-				nickName.setBackgroundColor(0xffcccccc);
-				
-				editProfile.setText("cancel");
-				
-			}else if(editProfile.getText()=="cancel")
-			{
-				tvUserName.setEnabled(false);
-				etEmail.setEnabled(false);
-				etSex.setEnabled(false);
-				tvUserName.setEnabled(false);
-				etDob.setEnabled(false);
-				etContact.setEnabled(false);
-				nickName.setEnabled(false);
-				
-				tvUserName.setBackgroundColor(0x00000000);
-				etEmail.setBackgroundColor(0x00000000);
-				etSex.setBackgroundColor(0x00000000);
-				
-				etDob.setBackgroundColor(0x00000000);
-				etContact.setBackgroundColor(0x00000000);
-				nickName.setBackgroundColor(0xffcccccc);
-				
-				editProfile.setText("edit profile");
-				btnUpdate.setVisibility(View.GONE);
-				btnClear.setVisibility(View.GONE);
+				if (etEmail.isEnabled() == false) {
+
+					btnUpdate.setVisibility(View.VISIBLE);
+					btnClear.setVisibility(View.VISIBLE);
+					tvUserName.setEnabled(true);
+					etEmail.setEnabled(true);
+					etSex.setEnabled(true);
+					tvUserName.setEnabled(true);
+					etDob.setEnabled(true);
+					etContact.setEnabled(true);
+					nickName.setEnabled(true);
+
+					tvUserName.setBackgroundColor(0xffcccccc);
+					etEmail.setBackgroundColor(0xffcccccc);
+					etSex.setBackgroundColor(0xffcccccc);
+					tvUserName.setBackgroundColor(0xffcccccc);
+					etDob.setBackgroundColor(0xffcccccc);
+					etContact.setBackgroundColor(0xffcccccc);
+					nickName.setBackgroundColor(0xffcccccc);
+
+					editProfile.setText("cancel");
+
+				} else if (editProfile.getText() == "cancel") {
+					tvUserName.setEnabled(false);
+					etEmail.setEnabled(false);
+					etSex.setEnabled(false);
+					tvUserName.setEnabled(false);
+					etDob.setEnabled(false);
+					etContact.setEnabled(false);
+					nickName.setEnabled(false);
+
+					tvUserName.setBackgroundColor(0x00000000);
+					etEmail.setBackgroundColor(0x00000000);
+					etSex.setBackgroundColor(0x00000000);
+
+					etDob.setBackgroundColor(0x00000000);
+					etContact.setBackgroundColor(0x00000000);
+					nickName.setBackgroundColor(0xffcccccc);
+
+					editProfile.setText("edit profile");
+					btnUpdate.setVisibility(View.GONE);
+					btnClear.setVisibility(View.GONE);
+				}
 			}
-			}});
-		
-		btnClear.setOnClickListener(new OnClickListener(){
+		});
+
+		btnClear.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				
-				
+
 				etEmail.setText("");
 				etSex.setText("");
-				
+
 				etDob.setText("");
 				etContact.setText("");
 				nickName.setText("");
-				
-				
+
 				etEmail.setHint("hint: abc@gmail.com");
 				etSex.setHint("hint: m/f");
-				
+
 				etDob.setHint("hint: 01/01/1991");
 				etContact.setHint("hint: 98765432");
 				nickName.setHint("hint: jack");
-				
+
 				tvUserName.setEnabled(true);
 				etEmail.setEnabled(true);
 				etSex.setEnabled(true);
@@ -138,51 +138,103 @@ public class ProfileActivity extends Activity {
 				etDob.setEnabled(true);
 				etContact.setEnabled(true);
 				nickName.setEnabled(true);
-				
+
 				tvUserName.setBackgroundColor(0xffcccccc);
 				etEmail.setBackgroundColor(0xffcccccc);
 				etSex.setBackgroundColor(0xffcccccc);
-				
+
 				etDob.setBackgroundColor(0xffcccccc);
 				etContact.setBackgroundColor(0xffcccccc);
 				nickName.setBackgroundColor(0xffcccccc);
-				
-			}});
 
-		
-		btnUpdate.setOnClickListener(new OnClickListener(){
+			}
+		});
+
+		btnUpdate.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+
 				String updateUserName, updateEmail, updateSex, updateDob, updateContact, updateNickName, updateImageUrl, updateStatus;
 				int userId;
 				userId = StaticObjects.getCurrentUser().getUserID();
-				
+
 				updateUserName = tvUserName.getText().toString();
 				updateEmail = etEmail.getText().toString();
-				//updateSex = etSex.getText().toString();
-				updateSex="testUpdate";
-				updateImageUrl ="testImage";
-				updateStatus="testStatus";
+				// updateSex = etSex.getText().toString();
+				updateSex = "testUpdate";
+				updateImageUrl = "testImage";
+				updateStatus = "testStatus";
 				updateDob = etDob.getText().toString();
 				updateContact = etContact.getText().toString();
 				updateNickName = nickName.getText().toString();
+
+				StaticObjects.getCurrentUser().setEmail(
+						etEmail.getText().toString().replace(" ", "%20"));
 				
-				StaticObjects.getCurrentUser().setEmail(etEmail.getText().toString().replace(" ", "%20"));
 				
 				
-			}});
-		
+
+			}
+		});
+
 	}
 
+	
+	public void updateUser()
+	{
+		if(StaticObjects.getCurrentUser()==null)
+		{
+		    new Thread(new Runnable() {
+				  @Override
+				  public void run()
+				  {
+					  	ExecutorService executor = Executors.newFixedThreadPool(1);
+					  	
+				       // UpdateUserRequest updateUserRequest = new UpdateUserRequest(etEmail.getText().toString(),etPassword2.getText().toString(),etNickName.getText().toString(),etContact.getText().toString(),etDOB.getText().toString(),"test", "test","test" );
+				          
+				 //       executor.execute(createUserRequest);
+						executor.shutdown();
+				        try {
+				        	executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+				       	  	Log.i(" RESPONSE :","ENDED REQUEST");
+				       	  	
+				        } catch (InterruptedException e) {}
+
+	                	  runOnUiThread(new Runnable() {
+	                          @Override
+	                          public void run()
+	                          {
+	                        	  staticObjects= new StaticObjects();
+	                        	  if(StaticObjects.getCurrentUser()==null)
+	                        	  {
+	                                    Log.i("USER", "NO USER");
+	                        	  }
+	                        	  else
+	                        	  {
+//	                        		  	User user = StaticObjects.getCurrentUser();
+//	                        		  	if(et_pass.getText().toString().equals(user.getPassword())){
+//	                        		  		validUser=true;        
+	                        		//  	}
+	                        	  }
+	                          }
+	                        });
+				  }
+				}).start();
+		}
+		else
+		{
+			Log.i("USER", "weird USER");
+		}
+		} 
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-	
-	
+
 }
